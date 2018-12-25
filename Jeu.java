5b02d7f9c17feb7723826227cbec4c672fea9a77
Thomas Copt-Bignon <lowx.add@gmail.com>
2018-12-25 22:47:19 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -5,5 +5,5 @@ public abstract class Jeu {
 	public abstract void lancerPartie(); // Lance et initialise la partie.
 	public abstract void joueUnTour(Joueur j); // Lance un tour pour le joueur correspondant.
 	public abstract void setJoueur(); // Mets en place les joueurs.
-	public abstract void afficheIntro();
+	public abstract void afficheIntro(); // Affiche un titre et les règles au début de la partie.
 }

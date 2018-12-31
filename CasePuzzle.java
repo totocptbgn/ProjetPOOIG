8c76abc6e7b50505bb7f35e2a9fadd924697f072
Thomas Copt-Bignon <lowx.add@gmail.com>
2018-12-31 16:52:09 +0100

    Avancement Puzzle

--- CasePuzzle.java
+++ CasePuzzle.java
@@ -1,5 +1,5 @@
 public class CasePuzzle extends Case {
-	int id; // Id de la case, la pièce posé dessus doit avoir le même id pour considérer qu'elle est à sa place !
+	private int id; // Id de la case, la pièce posé dessus doit avoir le même id pour considérer qu'elle est à sa place !
 
 	public CasePuzzle(int id, PiecePuzzle pz) {
 		this.id = id;
--- PiecePuzzle.java
+++ PiecePuzzle.java
@@ -1,9 +1,17 @@
 public class PiecePuzzle extends Piece {
-	String image; // Contient le chemin de l'image.
-	int id; // ID de la pièce qui la différencie des autres et qui informe sur quelle pièce se placer.
+	private String image; // Contient le chemin de l'image.
+	private int id; // ID de la pièce qui la différencie des autres et qui informe sur quelle pièce se placer.
 
 	public PiecePuzzle(String image, int id) {
 		this.image = image;
 		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getImage() {
+		return image;
 	}
 }

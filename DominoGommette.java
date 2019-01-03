c3f764a6a304cfb5b1a4ad206ea302de608aa3ab
Thomas Copt-Bignon <lowx.add@gmail.com>
2019-01-03 23:35:49 +0100

    Update DominoGommette.java

--- DominoGommette.java
+++ DominoGommette.java
@@ -1,16 +1,24 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 
 public class DominoGommette extends Domino {
 
 	// Cette version du domino est la même que celle de Domino.java mais
 	// les chiffres sont remplacés par des couleurs
 
+	// NE FONCTIONNE PAS /!\
+
 	private Color [] colors;
+
 	/* Tableau des couleurs, chaque couleurs fait références au chiffre à son index
 	 * dans le tableau :
 	 * 0 = Violet,
@@ -24,7 +32,56 @@ public class DominoGommette extends Domino {
 
 	@Override
 	public void lancerPartie() {
-		super.lancerPartie();
+		afficheIntro();
+
+		// Création des Joueurs
+		setJoueur();
+
+		// Création du plateau
+		this.plateau = new PlateauDomino(15);
+		this.premierTour = true;
+
+		// Création des Dominos
+		// Toutes les pièces qui existent.
+		ArrayList<PieceDomino> pieces = new ArrayList<>();
+		for (int i = 0; i < 7 ; i++ ) {
+			for (int j = i; j < 7 ; j++ ) {
+				pieces.add(new PieceDomino(i, j));
+			}
+		}
+
+		// Distribution des Dominos
+		this.paquet = new ArrayList[participants.length + 1];
+		for (int i = 0; i < paquet.length; i++) {
+			this.paquet[i] = new ArrayList<>();
+		}
+		Random rand = new Random();
+		int dist;
+		if (participants.length == 2){
+			dist = 7;
+		} else {
+			dist = 6;
+		}
+		for (int i = 1; i < paquet.length; i++) {
+			for (int j = 0; j < dist; j++) {
+				PieceDomino p = pieces.remove(rand.nextInt(pieces.size()));
+				p.setProprio(participants[i-1]);
+				this.paquet[i].add(p);
+			}
+		}
+		while(!pieces.isEmpty()){
+			this.paquet[0].add(pieces.remove(0));
+		}
+
+		// Affichage des dominos
+		System.out.println("Voici les dominos de chaque joueur :");
+		for (int i = 1; i < paquet.length; i++) {
+			System.out.print( participants[i-1].getNom() + " : ");
+			for (int j = 0; j < paquet[i].size(); j++) {
+				System.out.print(paquet[i].get(j) + " ");
+			}
+			System.out.println();
+		}
 
 		// Création des couleurs à utiliser
 		this.colors = new Color[7];
@@ -36,6 +93,76 @@ public class DominoGommette extends Domino {
 		colors[5] = Color.GREEN;
 		colors[6] = Color.BLUE;
 
+		while (true) {
+			for (Joueur participant : participants) {
+				if (!gagnant[participant.getId()]) {
+					joueUnTour(participant);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void setJoueur() { // Met en place la création des joueurs.
+
+		// On crée une fenêtre JFrame
+		JFrame frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+
+		// Emplacement pour afficher une question
+		JLabel text = new JLabel("");
+		frame.add(text);
+
+		// Emplacement pour y répondre
+		JTextField field = new JTextField(10);
+		frame.add(field);
+
+		// Bouton pour valider
+		JButton ok = new JButton("Ok");
+		frame.add(ok);
+
+		// Actualisation de la question et affichage de la fenêtre
+		text.setText("Combien de joueurs participent ? ");
+		frame.pack();
+		frame.setVisible(true);
+
+		place = 1;
+		final boolean[] b = {true};
+		while (b[0]){
+			ok.addActionListener((event) -> {
+				int nbrJ;
+				try {
+					String s = field.getText();
+					nbrJ = Integer.parseInt(s);
+					if (nbrJ < 2 || nbrJ > 4) {
+						text.setText("Combien de joueurs participent ? (Rentrez un chiffre entre 2 et 4 inclus.)");
+						frame.pack();
+					} else {
+						this.participants = new Joueur[nbrJ];
+						this.gagnant = new boolean[nbrJ];
+						for (int i = 0; i < gagnant.length; i++) {
+							this.gagnant[i] = false;
+						}
+						b[0] = false;
+					}
+				} catch (NumberFormatException e){
+					text.setText("Combien de joueurs participent ? Rentrez un nombre valide.");
+					frame.pack();
+				}
+			});
+		}
+
+		for (int i = 0; i < participants.length; i++) {
+			text.setText("Quel est le nom du joueur n°" + (i+1) + " ? ");
+			field.setText("");
+			final int id = i;
+			ok.addActionListener((e) ->{
+				participants[id] = new Joueur(field.getText(), id);
+			});
+		}
 	}
 
 	private class VueDominoGommette extends JFrame { // Classe interne qui gère l'interface graphique

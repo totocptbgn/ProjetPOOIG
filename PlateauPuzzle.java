a2d0cfb7a50270f42ec74c168933d2c77696a3b9
Thomas Copt-Bignon <lowx.add@gmail.com>
2019-01-04 22:23:59 +0100

    Le Rush

--- PlateauPuzzle.java
+++ PlateauPuzzle.java
@@ -19,7 +19,50 @@ public class PlateauPuzzle extends Plateau {
 
 	@Override
 	public void afficher() {
-		// Cette fonction est inutile ici car le jeu est entièrement en interface graphique ...
+		// Affichage de la grille
+		System.out.print("      ");
+		for (int j = 1; j < longueur + 1; j++) {
+			System.out.print("  ");
+			if (j < 10){
+				System.out.print(" " + j + " ");
+			} else {
+				System.out.print(" " + j);
+			}
+			System.out.print(" ");
+		}
+		System.out.println();
+		for (int i = 0; i < hauteur; i++) {
+			System.out.print("      ");
+			for (int j = 0; j < longueur; j++) {
+				System.out.print("+-----");
+			}
+			System.out.println("+");
+			if ((i + 1) < 10){
+				System.out.print("   " + (i + 1) + "  ");
+			} else {
+				System.out.print("  " + (i + 1) + "  ");
+			}
+			for (int j = 0; j < longueur; j++) {
+				System.out.print("|  ");
+				if (getCase(i, j).estOccupee()){
+					int val = ((PiecePuzzle) getCase(i, j).getPiece()).getId();
+					if (val != 8){
+						System.out.print(val);
+					} else {
+						System.out.print(" ");
+					}
+				} else {
+					System.out.print(" ");
+				}
+				System.out.print("  ");
+			}
+			System.out.println("|");
+		}
+		System.out.print("      ");
+		for (int j = 0; j < longueur; j++) {
+			System.out.print("+-----");
+		}
+		System.out.println("+");
 	}
 
 	public ArrayList<CasePuzzle> getAllCases(){ // Renvoie un ArrayList de toutes cases du plateau
--- Puzzle.java
+++ Puzzle.java
@@ -6,7 +6,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 /*
  * 				@author Thomas (Idée de Hind)
@@ -44,11 +46,143 @@ public class Puzzle extends Jeu {
 			}
 		}
 
-		// Lancement de l'interface graphique
-		EventQueue.invokeLater(() -> new VuePuzzle());
-
 		// On affiche le message
 		afficheIntro();
+
+		// On propose le choix de l'interface au joueur
+		boolean textuel = false;
+		boolean b = true;
+		while (b) {
+			System.out.println(" Une interface graphique est disponible ! (recommandée)");
+			System.out.print(" Voulez vous l'utiliser ? [y/n] ");
+			Scanner sc = new Scanner(System.in);
+			String answer = sc.next();
+			if (answer.equals("y") || answer.equals("Y")){
+				// Lancement de l'interface graphique
+				b = false;
+				EventQueue.invokeLater(() -> new VuePuzzle());
+			} else if (answer.equals("n") || answer.equals("N")){
+				textuel = true;
+				b = false;
+			} else {
+				System.err.println("...");
+			}
+		}
+
+		// Si il choisit l'interface textuel, on la lance
+		if (textuel){
+			System.out.println();
+			int i = 1;
+			while (true){
+				System.out.println(" Tour " + i + " :");
+				joueUnTour(participants[0]);
+				i++;
+			}
+		}
+	}
+
+	@Override
+	public void joueUnTour(Joueur j) {
+		// Afficher le puzzle
+		System.out.println();
+		plateau.afficher();
+		System.out.println();
+
+		// Demander au joueur quelle pièce il veut coulisser
+		System.out.println("Quelle pièce voullez-vous coullissez ?");
+		int ligne = 0;
+		int colonne = 0;
+		int input;
+		boolean b;
+		boolean c = true;
+
+		while (c){
+			// On demande la ligne
+			b = true;
+			while (b) {
+				System.out.print(" Donnez la ligne : ");
+				Scanner sc = new Scanner(System.in);
+				try {
+					input = sc.nextInt();
+					input--;
+					if (input >= 0 && input < plateau.hauteur){
+						ligne = input;
+						b = false;
+					} else {
+						System.err.println(" Rentrez un nombre correspondant à une ligne.");
+					}
+				} catch (InputMismatchException e){
+					System.err.println(" Rentrez un nombre valide.");
+					input = -1;
+				}
+			}
+
+			// On demande la colonne
+			b = true;
+			while (b) {
+				System.out.print(" Donnez la colonne : ");
+				Scanner sc = new Scanner(System.in);
+				try {
+					input = sc.nextInt();
+					input--;
+					if (input >= 0 && input < plateau.hauteur){
+						colonne = input;
+						b = false;
+					} else {
+						System.err.println(" Rentrez un nombre correspondant à une ligne.");
+					}
+				} catch (InputMismatchException e){
+					System.err.println(" Rentrez un nombre valide.");
+					input = -1;
+				}
+			}
+
+			// On récupére les case à la position :
+			int pieceID = -1;
+			if (ligne == 0){
+				if (colonne == 0) pieceID = 0;
+				if (colonne == 1) pieceID = 1;
+				if (colonne == 2) pieceID = 2;
+			} else if (ligne == 1){
+				if (colonne == 0) pieceID = 3;
+				if (colonne == 1) pieceID = 4;
+				if (colonne == 2) pieceID = 5;
+			} else if (ligne == 2){
+				if (colonne == 0) pieceID = 6;
+				if (colonne == 1) pieceID = 7;
+				if (colonne == 2) pieceID = 8;
+			}
+
+			// On récupère la case selectionnée
+			CasePuzzle cp = ((PlateauPuzzle) plateau).getAllCases().get(pieceID);
+
+			// On récupère la case vide
+			CasePuzzle cv = null;
+			ArrayList<CasePuzzle> a = ((PlateauPuzzle) plateau).getAllCases();
+			for (CasePuzzle casePuzzle : a) {
+				if (((PiecePuzzle) casePuzzle.getPiece()).getId() == 8) {
+					cv = casePuzzle;
+				}
+			}
+
+			if (!echangeCase(cp, cv)){
+				System.err.println("La cace indiquée n'est pas adjacente à la case vide !");
+			} else {
+				c = false;
+			}
+			System.out.println();
+			if (verifCase()){
+				System.out.println(" Bravo ! Vous avez résolu le puzzle !");
+				System.exit(0);
+			}
+		}
+	}
+
+	@Override
+	public void setJoueur() {
+		// Dans ce jeu le joueur est unique est n'as pas de réel utilité.
+		this.participants = new Joueur[1];
+		participants[0] = new Joueur("J1", 0);
 	}
 
 	private boolean verifCase(){
@@ -63,6 +197,100 @@ public class Puzzle extends Jeu {
 			}
 		}
 		return true;
+	}
+
+	private boolean echangeCase(CasePuzzle cp, CasePuzzle cv){
+		// Vérifier qu'elle soit en contact avec la pièce vide
+		int idClicked = cp.getValeur();
+		int idEmpty = cv.getValeur();
+
+		boolean areSided = false;
+
+		switch (idEmpty) {
+			case 0:
+				int [] isOk0 = {1, 3};
+				for (int i1 : isOk0) {
+					if (idClicked == i1) {
+						areSided = true;
+					}
+				}
+				break;
+			case 1:
+				int [] isOk1 = {0, 2, 4};
+				for (int i1 : isOk1) {
+					if (idClicked == i1) {
+						areSided = true;
+					}
+				}
+				break;
+			case 2:
+				int [] isOk2 = {1, 5};
+				for (int i1 : isOk2) {
+					if (idClicked == i1) {
+						areSided = true;
+					}
+				}
+				break;
+			case 3:
+				int [] isOk3 = {0, 4, 6};
+				for (int i1 : isOk3) {
+					if (idClicked == i1) {
+						areSided = true;
+					}
+				}
+				break;
+			case 4:
+				int [] isOk4 = {1, 3, 5, 7};
+				for (int i5 : isOk4) {
+					if (idClicked == i5) {
+						areSided = true;
+					}
+				}
+				break;
+			case 5:
+				int [] isOk5 = {2, 4, 8};
+				for (int i4 : isOk5) {
+					if (idClicked == i4) {
+						areSided = true;
+					}
+				}
+				break;
+			case 6:
+				int [] isOk6 = {3, 7};
+				for (int i3 : isOk6) {
+					if (idClicked == i3) {
+						areSided = true;
+					}
+				}
+				break;
+			case 7:
+				int [] isOk7 = {6, 4, 8};
+				for (int i2 : isOk7) {
+					if (idClicked == i2) {
+						areSided = true;
+					}
+				}
+				break;
+			case 8:
+				int [] isOk8 = {7, 5};
+				for (int i1 : isOk8) {
+					if (idClicked == i1) {
+						areSided = true;
+					}
+				}
+				break;
+		}
+
+		// Si c'est le cas échanger les pièces des cases
+		if (areSided){
+			PiecePuzzle pp = (PiecePuzzle) cp.getPiece();
+			PiecePuzzle pv = (PiecePuzzle) cv.getPiece();
+
+			cv.setPiece(pp);
+			cp.setPiece(pv);
+			return true;
+		}
+		return false;
 	}
 
 	private class VuePuzzle extends JFrame { // Classe interne qui gère l'interface graphique
@@ -106,100 +334,12 @@ public class Puzzle extends Jeu {
 						ArrayList<CasePuzzle> a = ((PlateauPuzzle) plateau).getAllCases();
 						CasePuzzle cv = null;
 						for (CasePuzzle casePuzzle : a) {
-							if (((PiecePuzzle) casePuzzle.getPiece()).getImage().equals("./img/Puzzle/PuzzleCut8.png")) {
+							if (((PiecePuzzle) casePuzzle.getPiece()).getId() == 8) {
 								cv = casePuzzle;
 							}
 						}
 
-						// Vérifier qu'elle soit en contact avec la pièce vide
-						int idClicked = cp.getValeur();
-						int idEmpty = cv.getValeur();
-
-						boolean areSided = false;
-
-						switch (idEmpty) {
-							case 0:
-								int [] isOk0 = {1, 3};
-								for (int i1 : isOk0) {
-									if (idClicked == i1) {
-										areSided = true;
-									}
-								}
-								break;
-							case 1:
-								int [] isOk1 = {0, 2, 4};
-								for (int i1 : isOk1) {
-									if (idClicked == i1) {
-										areSided = true;
-									}
-								}
-								break;
-							case 2:
-								int [] isOk2 = {1, 5};
-								for (int i1 : isOk2) {
-									if (idClicked == i1) {
-										areSided = true;
-									}
-								}
-								break;
-							case 3:
-								int [] isOk3 = {0, 4, 6};
-								for (int i1 : isOk3) {
-									if (idClicked == i1) {
-										areSided = true;
-									}
-								}
-								break;
-							case 4:
-								int [] isOk4 = {1, 3, 5, 7};
-								for (int i5 : isOk4) {
-									if (idClicked == i5) {
-										areSided = true;
-									}
-								}
-								break;
-							case 5:
-								int [] isOk5 = {2, 4, 8};
-								for (int i4 : isOk5) {
-									if (idClicked == i4) {
-										areSided = true;
-									}
-								}
-								break;
-							case 6:
-								int [] isOk6 = {3, 7};
-								for (int i3 : isOk6) {
-									if (idClicked == i3) {
-										areSided = true;
-									}
-								}
-								break;
-							case 7:
-								int [] isOk7 = {6, 4, 8};
-								for (int i2 : isOk7) {
-									if (idClicked == i2) {
-										areSided = true;
-									}
-								}
-								break;
-							case 8:
-								int [] isOk8 = {7, 5};
-								for (int i1 : isOk8) {
-									if (idClicked == i1) {
-										areSided = true;
-									}
-								}
-								break;
-						}
-
-						// Si c'est le cas échanger les pièces des cases
-						if (areSided){
-							PiecePuzzle pp = (PiecePuzzle) cp.getPiece();
-							PiecePuzzle pv = (PiecePuzzle) cv.getPiece();
-
-							cv.setPiece(pp);
-							cp.setPiece(pv);
-						}
+						echangeCase(cp, cv);
 
 						updateView();
 						// Si le joueur résout le puzzle
@@ -299,18 +439,6 @@ public class Puzzle extends Jeu {
 			g.drawImage(buffer,0,0,null);
 		}
 
-	}
-
-	@Override
-	public void joueUnTour(Joueur j) {
-		// Cette fonction est inutile ici car le jeu est joué par un seul joueur...
-	}
-
-	@Override
-	public void setJoueur() {
-		// Dans ce jeu le joueur est unique est n'as pas de réel utilité.
-		this.participants = new Joueur[1];
-		participants[0] = new Joueur("J1", 0);
 	}
 
 	@Override
